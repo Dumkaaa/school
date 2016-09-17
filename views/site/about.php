83cfce4f19a68d893422ce8ac1c160811c2150d3
Dmitry Larionov <dml@crtweb.ru>
2016-09-17 15:59:50 +0500

    second commit

--- views/site/about.php
+++ views/site/about.php
@@ -10,9 +10,6 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-      0!!!This is the About page. You may modify the following file to customize its content:
-    </p>
 
     <code><?= __FILE__ ?></code>
 </div>

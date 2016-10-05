3e3dc7022ab4f49b08ebe846cb33a903f5a491a7
Dmitriy Larionov <dml@crtweb.ru>
2016-10-05 18:45:54 +0500

    add access controll to admin

--- modules/admin/controllers/DefaultController.php
+++ modules/admin/controllers/DefaultController.php
@@ -3,7 +3,7 @@
 namespace app\modules\admin\controllers;
 
 use yii\web\Controller;
-
+use yii\filters\AccessControl;
 /**
  * Default controller for the `admin` module
  */
@@ -16,5 +16,32 @@ class DefaultController extends Controller
     public function actionIndex()
     {
         return $this->render('index');
+    }
+
+    /**
+     * Закрываем контроллер от неавторизованных юзеров
+     */
+    public function behaviors()
+    {
+        //получаем поведения, определенные в классе-родителе
+        $return = parent::behaviors();
+        //определяем свои поведения
+        $behaviors = [
+            //контролирует доступ к экшенам контроллера
+            'access' => [
+                //класс фильтра для доступа
+                'class' => AccessControl::className(),
+                //правила для доступа
+                'rules' => [
+                    [
+                        'allow' => true,
+                        //@ означает только авторизованных пользователей
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+        ];
+        //сливаем два массива в один и возвращаем
+        return array_merge($return, $behaviors);
     }
 }
